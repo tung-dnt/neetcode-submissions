13bb77e5d1f7a2c3db29a78459c9fb20515deeb0
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-07-27 12:47:34 +0700

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.go
@@ -0,0 +1,27 @@
+func longestConsecutive(nums []int) int {
+	// array to set
+	numSet := make(map[int]struct{})
+	for _, num := range nums {
+		numSet[num] = struct{}{}
+	}
+	
+	// count longest consecutive
+	longest := 0
+	for num := range numSet {
+		
+		if _, found := numSet[num - 1]; !found {
+			length := 1
+			for {
+				if _, exist := numSet[num + length]; !exist {
+					break
+				}
+				length++
+			}
+			
+			if longest < length {
+				longest = length
+			}
+		}
+	}
+	return longest
+}
