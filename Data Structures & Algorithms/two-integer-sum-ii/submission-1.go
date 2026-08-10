39b352394d985c1f45436f13291c9f127b70b1e1
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-08-10 21:42:05 +0700

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.go
@@ -0,0 +1,16 @@
+func twoSum(numbers []int, target int) []int {
+	l,r := 0, len(numbers)-1
+
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum == target {
+			return []int{l+1, r+1}
+		} else if sum < target {
+			l++
+		} else if sum > target {
+			r--
+		}
+	}
+
+	return []int{-1, -1}
+}
