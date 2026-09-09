4dce21147f0d82ce99007f1ce8f28d22889a205d
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-09 13:18:22 +0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.go
@@ -0,0 +1,15 @@
+func search(nums []int, target int) int {
+	l,r:=0,len(nums)-1
+	var mid int
+	for l<=r {
+		mid = (l+r)/2
+		if nums[mid] < target {
+			l=mid+1
+		} else if nums[mid] > target {
+			r=mid-1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
