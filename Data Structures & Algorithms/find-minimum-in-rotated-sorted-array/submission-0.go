f98cf868b4c01ee32b52803e67859afabb96e134
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-12 21:02:22 +0700

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.go
@@ -0,0 +1,14 @@
+func findMin(nums []int) int {
+	l,r:=0,len(nums)-1
+	for l <= r {
+		mid := (l+r)/2
+		if nums[mid] < nums[r] {
+			r = mid
+		} else if nums[mid] > nums[r] {
+			l = mid + 1
+		} else {
+			return nums[mid]
+		}
+	}
+	return -1
+}
