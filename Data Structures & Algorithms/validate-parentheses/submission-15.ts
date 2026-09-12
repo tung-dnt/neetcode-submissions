5cff2dc8cfb4823d7beccbea0f79e2962e5f1b4f
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-13 02:13:59 +0700

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const l: number = s.length;
+
+        const map = new Map();
+        map.set("{", "}");
+        map.set("[", "]");
+        map.set("(", ")");
+
+        if (l % 2 !== 0) return false;
+
+        const stack: string[] = [];
+
+        for (let i = 0; i < l; i++) {
+            if (s[i] === "{" || s[i] === "[" || s[i] === "(") stack.push(s[i]);
+            else {
+                if (stack.length === 0 || s[i] !== map.get(stack.pop())) return false;
+            }
+        }
+
+        if (stack.length > 0) return false;
+
+        return true;
+    }
+}
