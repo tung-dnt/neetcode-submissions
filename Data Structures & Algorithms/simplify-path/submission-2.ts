cc1c05029e13617e0f4e21e52941e118b6e076ea
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-16 11:40:45 +0700

    Add: simplify-path - submission-2

---
+++ Data Structures & Algorithms/simplify-path/submission-2.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} path
+     * @return {string}
+     */
+    simplifyPath(path: string): string {
+        const pathArr: string[] = path.split("/");
+        const stack: string[] = [];
+
+        for (let i of pathArr) {
+            if (i === "..") stack.pop();
+            else if (i !== "." && i !== "") stack.push(i);
+        }
+
+        return "/" + stack.join("/");
+    }
+}
