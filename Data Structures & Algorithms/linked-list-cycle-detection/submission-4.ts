c920b6b92d9bd94fe10fe1d112c1f93ab7b5a2f3
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-18 22:06:18 +0700

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head: ListNode | null): boolean {
+        const set = new Set();
+        let current = head;
+
+        while (current !== null) {
+            if (set.has(current)) return true;
+
+            set.add(current);
+            current = current.next;
+        }
+
+        return false;
+    }
+}
