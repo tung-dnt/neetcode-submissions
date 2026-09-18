47697dc7698e3de8cdce105f569a64d24802534f
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-19 00:03:08 +0700

    Add: remove-node-from-end-of-linked-list - submission-10

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-10.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} n
+     * @return {ListNode}
+     */
+    removeNthFromEnd(head: ListNode | null, n: number): ListNode {
+        let length: number = 0;
+        let current: ListNode = head;
+
+        while (current) {
+            length++;
+            current = current.next;
+        }
+
+        const removeIdx: number = length - n;
+
+        if (removeIdx === 0) return length === 1 ? null : head.next;
+
+        current = head;
+        let currentIdx: number = 0;
+
+        while (current) {
+            if (currentIdx === removeIdx - 1) {
+                current.next = current.next.next;
+                break;
+            }
+
+            current = current.next;
+            currentIdx++;
+        }
+
+        return head;
+    }
+}
