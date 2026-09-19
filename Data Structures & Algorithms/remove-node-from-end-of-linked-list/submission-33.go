aa9cbfb6575237bd6a52ba35473e7761e3d5dfcf
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-20 00:51:58 +0700

    Add: remove-node-from-end-of-linked-list - submission-33

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-33.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// 2 pointer
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Next: head}
+	// Nếu l start ở head thì khi dừng sẽ dừng đúng ngay vị trí cần remove
+	// Muốn replace thì phải nối Next của node trước vào node sau, nên phải lùi left về 1 node trước khi duyệt
+	// -> Tạo dummy node
+	l,r := dummy, head
+	for n != 0 {
+		r = r.Next
+		n--
+	}
+	for r != nil {
+		r = r.Next
+		l = l.Next
+	}
+	l.Next = l.Next.Next
+	return dummy.Next
+}
