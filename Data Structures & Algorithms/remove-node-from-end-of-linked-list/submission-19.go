4500bb754df232993330f1cdcf0d661528043d16
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-20 00:02:55 +0700

    Add: remove-node-from-end-of-linked-list - submission-19

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-19.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+import "slices"
+
+// Array converting - 6min
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	arr:=make([]int, 0)
+	cur:=head
+	for cur != nil  {
+		arr = append(arr, cur.Val)
+		cur = cur.Next
+	}
+
+	idxToDelete := len(arr)-n
+	arr = slices.Delete(arr, idxToDelete, idxToDelete+1)
+
+	dummyNode := &ListNode{}
+	curNode := dummyNode
+	for _, val := range arr {
+		newNode := &ListNode{Val: val}
+		curNode.Next = newNode
+		curNode = newNode
+	}
+
+	return dummyNode.Next
+}
