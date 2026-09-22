b84e2305fc4fe3b571c704263c675f2a8a8a7eab
tung_dnt <71514053+tung-dnt@users.noreply.github.com>
2026-09-22 16:51:28 +0700

    Add: remove-duplicates-from-sorted-array - submission-5

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-5.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums: number[]): number {
+        const set = new Set(nums);
+        
+        let i = 0;
+        set.forEach((n) => {
+            nums[i++] = n;
+        });
+
+        return set.size;
+    }
+}
